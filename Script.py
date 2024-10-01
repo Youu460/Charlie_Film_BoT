a7be5e531029c16698ed92aea3b0c2a80435fa29
🇮🇳 CØPY CØDE [Official] <164838401+MAX-COPYCODE-TG@users.noreply.github.com>
2024-10-02 00:58:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,2 +1,12 @@
 class script(object):
     START_TXT = """hello {}!!"""
+
+    LOG_TEXT_G = """#NewGroup
+Group = {}(<code>{}</code>)
+Total Members = <code>{}</code>
+Added By - {}
+"""
+    LOG_TEXT_P = """#NewUser
+ID - <code>{}</code>
+Name - {}
+"""
