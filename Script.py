98a1bf28ea1fab09fde650107d065b692c94cb3d
🇮🇳 CØPY CØDE [Official] <164838401+MAX-COPYCODE-TG@users.noreply.github.com>
2024-10-08 01:43:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,7 +3,9 @@ class script(object):
 𝘔𝘺 𝘕𝘢𝘮𝘦 𝘐𝘴 <a href=https://t.me/{}>{}</a>, 𝘗𝘳𝘰𝘷𝘪𝘥𝘦 𝘔𝘰𝘷𝘪𝘦𝘴, 𝘑𝘶𝘴𝘵 𝘈𝘥𝘥 𝘔𝘦 𝘛𝘰 𝘠𝘰𝘶𝘳 𝘎𝘳𝘰𝘶𝘱 𝘈𝘯𝘥 𝘌𝘯𝘫𝘰𝘺 😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """<i>➠ Owner: <a href=https://t.me/TG_x_filter>🇮🇳❍ 𝖒𝖆𝖓𝖙𝖎𝖘 ❍™◢ ◤</a>
+    ABOUT_TXT = """<b><u>⚠️ABOUT ME⚠️</u></b>
+
+<i>➠ Owner: <a href=https://t.me/TG_x_filter>🇮🇳❍ 𝖒𝖆𝖓𝖙𝖎𝖘 ❍™◢ ◤</a>
 ➠ Maintained By: <a href=tg://settings>My Friend</a>
 ➠ Library: Pyrogram 
 ➠ Language: Python3 
@@ -74,13 +76,13 @@ coммand and υѕeѕ
 • /users - <code>ᴛᴏ ɢᴇᴛ ʟɪꜱᴛ ᴏꜰ ᴍʏ ᴜꜱᴇʀꜱ ᴀɴᴅ ɪᴅꜱ.</code>
 • /chats - <code>ᴛᴏ ɢᴇᴛ ʟɪꜱᴛ ᴏꜰ ᴍʏ ᴄʜᴀᴛꜱ ᴀɴᴅ ɪᴅꜱ</code>
 • /channel - <code>ᴛᴏ ɢᴇᴛ ʟɪꜱᴛ ᴏꜰ ᴛᴏᴛᴀʟ ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴄʜᴀɴɴᴇʟꜱ</code>"""
-    STATUS_TXT = """<b>╭──᛬⚡️sᴛᴀᴛᴜs ʙᴏᴀʀᴅ⚡️᛬─╮
-├╼<b> 📂 ᴛᴏᴛᴀʟ ғɪʟᴇs : <code>{}</code></b>
-├╼<b> 🌐 ᴛᴏᴛᴀʟ ᴜsᴇʀs : <code>{}</code></b>
-├╼<b> 🔮 ᴛᴏᴛᴀʟ ᴄʜᴀᴛs : <code>{}</code></b>
-├╼<b> 💥 ᴜsᴇᴅ sᴛᴏʀᴀɢᴇ : <code>{}</code> 𝙼𝙱</b>
-├╼<b> 🎃 ғʀᴇᴇ sᴛᴏʀᴀɢᴇ : <code>{}</code> 𝙼𝙱</b>
-╰────────────────╯</b>"""
+    STATUS_TXT = """<b><u>📊 STATUS BOARD 📊</u></b>
+
+<i>➥ Total Files: <code>{}</code></i>
+<i>➥ Total Members: <code>{}</code></i>
+<i>➥ Total Chats: <code>{}</code></i>
+<i>➥ Used Storage: <code>{}</code> MB</i>"""
+    
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
