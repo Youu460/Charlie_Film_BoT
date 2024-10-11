98c4b1faf7f9608e94d5d2bcbedabe9de9ae2f10
🇮🇳 CØPY CØDE [Official] <164838401+MAX-COPYCODE-TG@users.noreply.github.com>
2024-10-12 03:44:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -805,6 +805,7 @@ async def advantage_spell_chok(msg):
     g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
+        google = search.replace(' ', "+")
         button = [[InlineKeyboardButton("🕵️‍♂️ Search On Google 🕵️‍♂️", url=f"https://google.com/search?q={google}")]]
         k = await msg.reply(f"<b>Hey 👋{message.from_user.mention}</b>\n\n<i>I couldn't find the file you requested 😕</i>\n<i>Try to do the following...</i>\n\n=> <i>Request with correct spelling</i>\n\n=> <i>Don't ask movies that are not released in OTT platforms</i>\n\n=> <i>Try to ask in [MovieName, Language] this format.</i>\n\n=> <i>Use the button below to search on Google 😌</i>", reply_markup=InlineKeyboardMarkup(button))
         await asyncio.sleep(8)
@@ -835,6 +836,7 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
+        google = search.replace(' ', "+")
         button = [[InlineKeyboardButton("🕵️‍♂️ Search On Google 🕵️‍♂️", url=f"https://google.com/search?q={google}")]]
         k = await msg.reply(f"<b>Hey 👋{message.from_user.mention}</b>\n\n<i>I couldn't find the file you requested 😕</i>\n<i>Try to do the following...</i>\n\n=> <i>Request with correct spelling</i>\n\n=> <i>Don't ask movies that are not released in OTT platforms</i>\n\n=> <i>Try to ask in [MovieName, Language] this format.</i>\n\n=> <i>Use the button below to search on Google 😌</i>", reply_markup=InlineKeyboardMarkup(button))
         await asyncio.sleep(8)
