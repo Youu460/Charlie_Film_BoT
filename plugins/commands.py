0f20f890321e882ce72795170cefb13b0ddaa18e
🇮🇳 CØPY CØDE [Official] <164838401+MAX-COPYCODE-TG@users.noreply.github.com>
2025-02-23 02:16:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -303,7 +303,7 @@ async def delete(bot, message):
     """Delete file from database"""
     reply = message.reply_to_message
     if reply and reply.media:
-        msg = await message.reply("Wᴀɪᴛ Pᴀɴɴᴜɴɢᴀ...⏳", quote=True)
+        msg = await message.reply("Processing...⏳", quote=True)
     else:
         await message.reply('Reply to file with /delete which you want to delete', quote=True)
         return
