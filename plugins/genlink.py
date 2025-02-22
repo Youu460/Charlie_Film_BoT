30fb0f78bceb56bf7ac9584acccd8820d72762c1
🇮🇳 CØPY CØDE [Official] <164838401+MAX-COPYCODE-TG@users.noreply.github.com>
2025-02-23 02:35:37 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -1,5 +1,5 @@
 import re
-from pyrogram import filters, Client, enums
+from pyrogram import filters, Client
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, UsernameInvalid, UsernameNotModified
 from info import ADMINS, LOG_CHANNEL, FILE_STORE_CHANNEL, PUBLIC_FILE_STORE
 from database.ia_filterdb import unpack_new_file_id
@@ -20,24 +20,24 @@ async def allowed(_, __, message):
         return True
     return False
 
-@Client.on_message(filters.command(['link', 'plink']) & filters.create(allowed))
+@Client.on_message(filters.command(['link', 'plink']) & filters.create(allowed) & filters.user(ADMINS))
 async def gen_link_s(bot, message):
     replied = message.reply_to_message
     if not replied:
-        return await message.reply('SʜᴀʀᴇAʙʟᴇ Lɪɴᴋ Gᴇɴᴇʀᴀᴛᴇ Pᴀɴɴᴜʀᴀᴛʜᴜᴋᴜ Eᴛʜᴀᴄʜɪ Mᴇssᴀɢᴇ Kᴜ Rᴇᴘʟʏ Pᴀɴɴᴜɴɢᴀ.')
+        return await message.reply('Reply to a message to get a shareable link.')
     file_type = replied.media
-    if file_type not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.AUDIO, enums.MessageMediaType.DOCUMENT]:
-        return await message.reply("Sᴜᴘᴘᴏʀᴛ Aɢᴜʀᴀ Fᴏʀᴍᴀᴛ Kᴜ Mᴀᴛᴛᴜᴍ Rᴇᴘʟʏ Pᴀɴɴᴜɴɢᴀ")
+    if file_type not in ["video", 'audio', 'document']:
+        return await message.reply("Reply to a supported media")
     if message.has_protected_content and message.chat.id not in ADMINS:
         return await message.reply("okDa")
-    file_id, ref = unpack_new_file_id((getattr(replied, file_type.value)).file_id)
+    file_id, ref = unpack_new_file_id((getattr(replied, file_type)).file_id)
     string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
     string += file_id
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
     await message.reply(f"Here is your Link:\nhttps://t.me/{temp.U_NAME}?start={outstr}")
     
     
-@Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
+@Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed) & filters.user(ADMINS))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
         return await message.reply("Use correct format.\nExample <code>/batch https://t.me/TeamEvamaria/10 https://t.me/TeamEvamaria/20</code>.")
@@ -63,23 +63,23 @@ async def gen_link_batch(bot, message):
         l_chat_id  = int(("-100" + l_chat_id))
 
     if f_chat_id != l_chat_id:
-        return await message.reply("Cʜᴀᴛ ɪᴅs Mᴀᴛᴄʜ Aɢᴀʟᴀ.")
+        return await message.reply("Chat ids not matched.")
     try:
         chat_id = (await bot.get_chat(f_chat_id)).id
     except ChannelInvalid:
-        return await message.reply('Iɴᴛʜᴀ Cʜᴀɴɴᴇʟ / Gʀᴏᴜᴘ Pʀɪᴠᴀᴛᴇ Aʜ Iʀᴜᴋᴀʟʟᴀᴍ. Pʀɪᴠᴀᴛᴇ Cʜᴀɴɴᴇʟ / Gʀᴏᴜᴘ Yᴇɴɴᴀ Aᴅᴍɪɴ Aᴋɪ Fɪʟᴇs Iɴᴅᴇx Pᴀɴɴɪᴋᴏɴɢᴀ')
+        return await message.reply('This may be a private channel / group. Make me an admin over there to index the files.')
     except (UsernameInvalid, UsernameNotModified):
         return await message.reply('Invalid Link specified.')
     except Exception as e:
         return await message.reply(f'Errors - {e}')
 
-    sts = await message.reply("Uɴɢᴀ Mᴇssᴀɢᴇ Kᴜ Lɪɴᴋ Gᴇɴᴇʀᴀᴛᴇ Pᴀɴɴᴜʀᴏᴍ.\nTʜɪs ᴍᴀʏ ᴛᴀᴋᴇ ᴛɪᴍᴇ ᴅᴇᴘᴇɴᴅɪɴɢ ᴜᴘᴏɴ ɴᴜᴍʙᴇʀ ᴏғ ᴍᴇssᴀɢᴇs")
+    sts = await message.reply("Generating link for your message.\nThis may take time depending upon number of messages")
     if chat_id in FILE_STORE_CHANNEL:
         string = f"{f_msg_id}_{l_msg_id}_{chat_id}_{cmd.lower().strip()}"
         b_64 = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
         return await sts.edit(f"Here is your link https://t.me/{temp.U_NAME}?start=DSTORE-{b_64}")
 
-    FRMT = "Gᴇɴᴀʀᴀᴛɪɴɢ Lɪɴᴋ...\nTotal Messages: `{total}`\nDone: `{current}`\nRemaining: `{rem}`\nStatus: `{sts}`"
+    FRMT = "Generating Link...\nTotal Messages: `{total}`\nDone: `{current}`\nRemaining: `{rem}`\nStatus: `{sts}`"
 
     outlist = []
 
@@ -95,7 +95,7 @@ async def gen_link_batch(bot, message):
             continue
         try:
             file_type = msg.media
-            file = getattr(msg, file_type.value)
+            file = getattr(msg, file_type)
             caption = getattr(msg, 'caption', '')
             if caption:
                 caption = caption.html
@@ -114,7 +114,7 @@ async def gen_link_batch(bot, message):
             pass
         if not og_msg % 20:
             try:
-                await sts.edit(FRMT.format(total=l_msg_id-f_msg_id, current=tot, rem=((l_msg_id-f_msg_id) - tot), sts="Sᴀᴠɪɴɢ Mᴇssᴀɢᴇs"))
+                await sts.edit(FRMT.format(total=l_msg_id-f_msg_id, current=tot, rem=((l_msg_id-f_msg_id) - tot), sts="Saving Messages"))
             except:
                 pass
     with open(f"batchmode_{message.from_user.id}.json", "w+") as out:
