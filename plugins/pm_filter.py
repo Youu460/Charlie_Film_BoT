773861e3e340c0f2659152795ba50f93eb247221
🇮🇳 CØPY CØDE [Official] <164838401+MAX-COPYCODE-TG@users.noreply.github.com>
2025-02-23 03:13:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -602,7 +602,6 @@ async def auto_filter(client, msg, spoll=False):
             search = message.text
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
             if not files:
-                await m.delete()
                 if settings["spell_check"]:
                     return await advantage_spell_chok(msg)
                 else:
