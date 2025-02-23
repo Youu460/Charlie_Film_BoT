4ccdd6f585e9ea391a64d6037ac2e3b886cb053a
🇮🇳 CØPY CØDE [Official] <164838401+MAX-COPYCODE-TG@users.noreply.github.com>
2025-02-23 20:06:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -77,7 +77,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f" [{get_size(file.file_size)}]  {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"▢ ⟮ {get_size(file.file_size)} ⟯ ▷  {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -617,7 +617,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f" [{get_size(file.file_size)}]  {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"▢ ⟮ {get_size(file.file_size)} ⟯ ▷  {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
