138a44222ef6495e2273d38376fae23fc5112e52
🇮🇳 CØPY CØDE [Official] <164838401+MAX-COPYCODE-TG@users.noreply.github.com>
2025-03-08 01:15:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,6 +39,9 @@ DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
+# Links
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/vpcreatz_film_mainia')
+
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
